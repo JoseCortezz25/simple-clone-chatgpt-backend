c8ec5d69a27c5f58f75cc092af85f7fc7dbc760b
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2023-02-12 13:48:35 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+const { Configuration, OpenAIApi } = require('openai')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/prompt', async (req, res) => {
+  const configuration = new Configuration({
+    organization: "org-mRa5t0KWGRiD58SsZqWUwTCA",
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  
+  const openai = new OpenAIApi(configuration)
+  const prompt = req.body.text;
+  
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${prompt}`,
+    max_tokens: 2048,
+    temperature: 0.5
+  });
+
+
+  res.json({
+    message: completion.data.choices[0].text
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
+});
