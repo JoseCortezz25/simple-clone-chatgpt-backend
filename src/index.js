c085fbc273d92d633a54989760a9a20e0e005403
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2023-02-18 20:32:18 -0500

    Added option to generate image from a prompt

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const configuration = new Configuration({
+  organization: "org-mRa5t0KWGRiD58SsZqWUwTCA",
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
 app.post('/api/prompt', async (req, res) => {
   console.log('Prompt recibido');
-  const configuration = new Configuration({
-    organization: "org-mRa5t0KWGRiD58SsZqWUwTCA",
-    apiKey: process.env.OPENAI_API_KEY,
-  })
-  
-  const openai = new OpenAIApi(configuration)
+
   const prompt = req.body.text;
-  
+
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${prompt}`,
@@ -27,11 +29,32 @@ app.post('/api/prompt', async (req, res) => {
     temperature: 0.5
   });
 
-  
+
   console.log('Respuesta enviada');
   res.json({
     message: completion.data.choices[0].text
   })
+});
+
+app.post('/api/image/prompt', async (req, res) => {
+  try {
+    const prompt = req.body.text;
+    console.log('📦 prompt:', prompt);
+    
+    const response = await openai.createImage({
+      prompt: `The drawing must have the style "pencil sketch storyboard" and must follow the following instruction: ${prompt}`,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    res.json({
+      image: response.data.data[0].url
+    })
+  } catch (error) {
+    res.status(error.response.status).json({
+      message: error.response.data.error
+    })
+  }
 });
 
 app.listen(port, () => {
